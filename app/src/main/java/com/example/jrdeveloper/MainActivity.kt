dc264b9c7a2532c52cfa0c53a41423c7914eff7a
RoyMcqueen <roy.mcqueen@romtech.com>
2024-10-30 10:06:46 -0500

    added Banking class

--- app/src/main/java/com/example/jrdeveloper/MainActivity.kt
+++ app/src/main/java/com/example/jrdeveloper/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.jrdeveloper
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -27,6 +28,8 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        startActivity(Intent(this,Banking::class.java))
     }
 }
 
